f90a38a977977d26f9af5829ede3338e8bf378db
noɿA <aron@eideldev.com>
2026-04-20 11:42:12 -0700

    Refactor os.system call to use f-string for filename

--- python-command-injection/burner.py
+++ python-command-injection/burner.py
@@ -4,7 +4,7 @@ import sys
 class Burner:
     def burn(self, filename):
         print("Burning now")
-        os.system("cat {filename} > /dev/sda")
+        os.system(f"cat {filename} > /dev/sda")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
